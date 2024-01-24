9f118a5515659753edd8d153c7518b7332530369
therealone989 <mrfabigm@gmail.com>
2024-01-24 09:02:36 +0100

    S

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
 <div>
 <nav className="nav navbar-dark bg-blue ">
-  <a className="nav-link active" aria-current="page" href="#">Active</a>
+  <a className="nav-link active" aria-current="page" href="#">Actives</a>
   <a className="nav-link" href="#">Link</a>
   <a className="nav-link" href="#">Link</a>
   <a className="nav-link disabled" aria-disabled="true">Disabled</a>
